45d6f9a5713e1a1817ecc2a69b4eb433cc638aa8
TakuyaAsanuma <bi912424@jp.ibm.com>
2018-10-12 20:57:12 +0900

    Update app.py

--- app.py
+++ app.py
@@ -44,6 +44,10 @@ from linebot.models import (
     SeparatorComponent, QuickReply, QuickReplyButton
 )
 
+import urllib.request
+import urllib.parse
+import json
+
 app = Flask(__name__)
 
 # get channel_secret and channel_access_token from your environment variable
@@ -61,6 +65,9 @@ handler = WebhookHandler(channel_secret)
 
 static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
 
+@app.route("/")
+def hello_world():
+    return "hello world!"
 
 # function for create tmp dir for download content
 def make_static_tmp_dir():
@@ -72,9 +79,6 @@ def make_static_tmp_dir():
         else:
             raise
 
-@app.route("/")
-def hello_world():
-    return "hello world!"
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -88,7 +92,7 @@ def callback():
     # handle webhook body
     try:
         handler.handle(body, signature)
-    except linebotApiError as e:
+    except LineBotApiError as e:
         print("Got exception from LINE Messaging API: %s\n" % e.message)
         for m in e.error.details:
             print("  %s: %s" % (m.property, m.message))
@@ -103,52 +107,7 @@ def callback():
 def handle_text_message(event):
     text = event.message.text
 
-    if text == 'profile':
-        if isinstance(event.source, SourceUser):
-            profile = line_bot_api.get_profile(event.source.user_id)
-            line_bot_api.reply_message(
-                event.reply_token, [
-                    TextSendMessage(text='Display name: ' + profile.display_name),
-                    TextSendMessage(text='Status message: ' + profile.status_message)
-                ]
-            )
-        else:
-            line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text="Bot can't use profile API without user ID"))
-    elif text == 'bye':
-        if isinstance(event.source, SourceGroup):
-            line_bot_api.reply_message(
-                event.reply_token, TextSendMessage(text='Leaving group'))
-            line_bot_api.leave_group(event.source.group_id)
-        elif isinstance(event.source, SourceRoom):
-            line_bot_api.reply_message(
-                event.reply_token, TextSendMessage(text='Leaving group'))
-            line_bot_api.leave_room(event.source.room_id)
-        else:
-            line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text="Bot can't leave from 1:1 chat"))
-    elif text == 'confirm':
-        confirm_template = ConfirmTemplate(text='Do it?', actions=[
-            MessageAction(label='Yes', text='Yes!'),
-            MessageAction(label='No', text='No!'),
-        ])
-        template_message = TemplateSendMessage(
-            alt_text='Confirm alt text', template=confirm_template)
-        line_bot_api.reply_message(event.reply_token, template_message)
-    elif text == 'buttons':
-        buttons_template = ButtonsTemplate(
-            title='My buttons sample', text='Hello, my buttons', actions=[
-                URIAction(label='Go to line.me', uri='https://line.me'),
-                PostbackAction(label='ping', data='ping'),
-                PostbackAction(label='ping with text', data='ping', text='ping'),
-                MessageAction(label='Translate Rice', text='米')
-            ])
-        template_message = TemplateSendMessage(
-            alt_text='Buttons alt text', template=buttons_template)
-        line_bot_api.reply_message(event.reply_token, template_message)
-    elif text == 'carousel':
+    if text == 'carousel':
         carousel_template = CarouselTemplate(columns=[
             CarouselColumn(text='hoge1', title='fuga1', actions=[
                 URIAction(label='Go to line.me', uri='https://line.me'),
@@ -161,275 +120,163 @@ def handle_text_message(event):
         ])
         template_message = TemplateSendMessage(
             alt_text='Carousel alt text', template=carousel_template)
+        print(template_message)
         line_bot_api.reply_message(event.reply_token, template_message)
-    elif text == 'image_carousel':
-        image_carousel_template = ImageCarouselTemplate(columns=[
-            ImageCarouselColumn(image_url='https://via.placeholder.com/1024x1024',
-                                action=DatetimePickerAction(label='datetime',
-                                                            data='datetime_postback',
-                                                            mode='datetime')),
-            ImageCarouselColumn(image_url='https://via.placeholder.com/1024x1024',
-                                action=DatetimePickerAction(label='date',
-                                                            data='date_postback',
-                                                            mode='date'))
-        ])
-        template_message = TemplateSendMessage(
-            alt_text='ImageCarousel alt text', template=image_carousel_template)
-        line_bot_api.reply_message(event.reply_token, template_message)
-    elif text == 'imagemap':
-        pass
-    elif text == 'flex':
-        bubble = BubbleContainer(
-            direction='ltr',
-            hero=ImageComponent(
-                url='https://example.com/cafe.jpg',
-                size='full',
-                aspect_ratio='20:13',
-                aspect_mode='cover',
-                action=URIAction(uri='http://example.com', label='label')
-            ),
-            body=BoxComponent(
-                layout='vertical',
-                contents=[
-                    # title
-                    TextComponent(text='Brown Cafe', weight='bold', size='xl'),
-                    # review
-                    BoxComponent(
-                        layout='baseline',
-                        margin='md',
-                        contents=[
-                            IconComponent(size='sm', url='https://example.com/gold_star.png'),
-                            IconComponent(size='sm', url='https://example.com/grey_star.png'),
-                            IconComponent(size='sm', url='https://example.com/gold_star.png'),
-                            IconComponent(size='sm', url='https://example.com/gold_star.png'),
-                            IconComponent(size='sm', url='https://example.com/grey_star.png'),
-                            TextComponent(text='4.0', size='sm', color='#999999', margin='md',
-                                          flex=0)
-                        ]
-                    ),
-                    # info
-                    BoxComponent(
-                        layout='vertical',
-                        margin='lg',
-                        spacing='sm',
-                        contents=[
-                            BoxComponent(
-                                layout='baseline',
-                                spacing='sm',
-                                contents=[
-                                    TextComponent(
-                                        text='Place',
-                                        color='#aaaaaa',
-                                        size='sm',
-                                        flex=1
-                                    ),
-                                    TextComponent(
-                                        text='Shinjuku, Tokyo',
-                                        wrap=True,
-                                        color='#666666',
-                                        size='sm',
-                                        flex=5
-                                    )
-                                ],
-                            ),
-                            BoxComponent(
-                                layout='baseline',
-                                spacing='sm',
-                                contents=[
-                                    TextComponent(
-                                        text='Time',
-                                        color='#aaaaaa',
-                                        size='sm',
-                                        flex=1
-                                    ),
-                                    TextComponent(
-                                        text="10:00 - 23:00",
-                                        wrap=True,
-                                        color='#666666',
-                                        size='sm',
-                                        flex=5,
-                                    ),
-                                ],
-                            ),
-                        ],
-                    )
-                ],
-            ),
-            footer=BoxComponent(
-                layout='vertical',
-                spacing='sm',
-                contents=[
-                    # callAction, separator, websiteAction
-                    SpacerComponent(size='sm'),
-                    # callAction
-                    ButtonComponent(
-                        style='link',
-                        height='sm',
-                        action=URIAction(label='CALL', uri='tel:000000'),
-                    ),
-                    # separator
-                    SeparatorComponent(),
-                    # websiteAction
-                    ButtonComponent(
-                        style='link',
-                        height='sm',
-                        action=URIAction(label='WEBSITE', uri="https://example.com")
-                    )
-                ]
-            ),
-        )
-        message = FlexSendMessage(alt_text="hello", contents=bubble)
-        line_bot_api.reply_message(
-            event.reply_token,
-            message
-        )
-    elif text == 'quick_reply':
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(
-                text='Quick reply',
-                quick_reply=QuickReply(
-                    items=[
-                        QuickReplyButton(
-                            action=PostbackAction(label="label1", data="data1")
-                        ),
-                        QuickReplyButton(
-                            action=MessageAction(label="label2", text="text2")
-                        ),
-                        QuickReplyButton(
-                            action=DatetimePickerAction(label="label3",
-                                                        data="data3",
-                                                        mode="date")
-                        ),
-                        QuickReplyButton(
-                            action=CameraAction(label="label4")
-                        ),
-                        QuickReplyButton(
-                            action=CameraRollAction(label="label5")
-                        ),
-                        QuickReplyButton(
-                            action=LocationAction(label="label6")
-                        ),
-                    ])))
-    else:
-        line_bot_api.reply_message(
-            event.reply_token, TextSendMessage(text=event.message.text))
-
 
 @handler.add(MessageEvent, message=LocationMessage)
 def handle_location_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        LocationSendMessage(
-            title=event.message.title, address=event.message.address,
-            latitude=event.message.latitude, longitude=event.message.longitude
-        )
-    )
+    try:
+#        print('start')
+        latitude=str(event.message.latitude)
+        longitude=str(event.message.longitude)
+        my = gnavi_api(latitude,longitude)
+        rests = do_json(my)
+        template_message = sendRest(rests)
+#        print(template_message)
+        line_bot_api.reply_message(event.reply_token, template_message)
+    except Exception as e:
+        print(Exception)
 
 
-@handler.add(MessageEvent, message=StickerMessage)
-def handle_sticker_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        StickerSendMessage(
-            package_id=event.message.package_id,
-            sticker_id=event.message.sticker_id)
-    )
+def gnavi_api(latitude,longitude):
+    key = '3f1eb1a6047e3a0a5a4c8120655bafcd'
+    url = "https://api.gnavi.co.jp/RestSearchAPI/v3/"
+    search_range = '2'
+    category_l = 'RSFST09000'
+    params = urllib.parse.urlencode({
+        'keyid': key,
+        'latitude': latitude,
+        'longitude': longitude,
+        'range':search_range,
+        'category_l':category_l
+    })
+    try:
+        responce = urllib.request.urlopen(url + '?' + params)
+        return responce.read()
+    except:
+        raise Exception(u'APIアクセスに失敗しました')
+
+def do_json(data):
+    parsed_data = json.loads(data)
+    if 'error' in parsed_data:
+        if 'message' in parsed_data:
+            raise Exception(u'{0}'.format(parsed_data['message']))
+        else:
+            raise Exception(u'データ取得に失敗しました')
+    total_hit_count = parsed_data.get('total_hit_count', 0)
+
+    if total_hit_count < 1:
+        raise Exception(u'指定した内容ではヒットしませんでした\nレストランデータが存在しなかったため終了します')
+    rests = parsed_data['rest']
+    return rests
+
+def sendRest(rests):
+    # for rest in rests:
+    #     carousel_template = CarouselTemplate(columns=[
+    #         CarouselColumn(text=rest['opentime'], title=rest['name'], actions=[
+    #             URIAction(label='webで検索', uri=rest['url_mobile'])
+    #         ]),
+    #     ])
+    opentime1 = rests[0]['opentime']
+    name1 = rests[0]['name']
+    address1 = rests[0]['address']
+    url1 = rests[0]['url_mobile']
+    thumbnail1 = rests[0]['image_url']['shop_image1']
+    tel1 = 'tel:'+rests[0]['tel']
+    
+    opentime2 = rests[1]['opentime']
+    name2 = rests[1]['name']
+    address2 = rests[1]['address']
+    url2 = rests[1]['url_mobile']
+    thumbnail2 = rests[1]['image_url']['shop_image1']
+    tel2 = 'tel:'+rests[1]['tel']
+
+    opentime3 = rests[2]['opentime']
+    name3 = rests[2]['name']
+    address3 = rests[2]['address']
+    url3 = rests[2]['url_mobile']
+    thumbnail3 = rests[2]['image_url']['shop_image1']
+    tel3 = 'tel:'+rests[2]['tel']
+
+    if(opentime1 == ''):
+        opentime1 = '営業時間：情報がありません'
+    if(name1 == ''):
+        name1 = '情報がありません'
+    if(address1 == ''):
+        address1 = '住所：情報がありません'
+    if(url1 == ''):
+        url1 = 'https://line.me'
+    if(thumbnail1 == ''):
+        thumbnail1 = 'https://example.com/cafe.jpg'
 
 
-# Other Message Type
-@handler.add(MessageEvent, message=(ImageMessage, VideoMessage, AudioMessage))
-def handle_content_message(event):
-    if isinstance(event.message, ImageMessage):
-        ext = 'jpg'
-    elif isinstance(event.message, VideoMessage):
-        ext = 'mp4'
-    elif isinstance(event.message, AudioMessage):
-        ext = 'm4a'
-    else:
-        return
-
-    message_content = line_bot_api.get_message_content(event.message.id)
-    with tempfile.NamedTemporaryFile(dir=static_tmp_path, prefix=ext + '-', delete=False) as tf:
-        for chunk in message_content.iter_content():
-            tf.write(chunk)
-        tempfile_path = tf.name
-
-    dist_path = tempfile_path + '.' + ext
-    dist_name = os.path.basename(dist_path)
-    os.rename(tempfile_path, dist_path)
-
-    line_bot_api.reply_message(
-        event.reply_token, [
-            TextSendMessage(text='Save content.'),
-            TextSendMessage(text=request.host_url + os.path.join('static', 'tmp', dist_name))
-        ])
+    if(opentime2 == ''):
+        opentime2 = '営業時間：情報がありません'
+    if(name2 == ''):
+        name2 = '情報がありません'
+    if(address2 == ''):
+        address2 = '住所：情報がありません'    
+    if(url2 == ''):
+        url2 = 'https://line.me'
+    if(thumbnail2 == ''):
+        thumbnail2 = 'https://example.com/cafe.jpg'
 
 
-@handler.add(MessageEvent, message=FileMessage)
-def handle_file_message(event):
-    message_content = line_bot_api.get_message_content(event.message.id)
-    with tempfile.NamedTemporaryFile(dir=static_tmp_path, prefix='file-', delete=False) as tf:
-        for chunk in message_content.iter_content():
-            tf.write(chunk)
-        tempfile_path = tf.name
-
-    dist_path = tempfile_path + '-' + event.message.file_name
-    dist_name = os.path.basename(dist_path)
-    os.rename(tempfile_path, dist_path)
-
-    line_bot_api.reply_message(
-        event.reply_token, [
-            TextSendMessage(text='Save file.'),
-            TextSendMessage(text=request.host_url + os.path.join('static', 'tmp', dist_name))
-        ])
-
-
-@handler.add(FollowEvent)
-def handle_follow(event):
-    line_bot_api.reply_message(
-        event.reply_token, TextSendMessage(text='Got follow event'))
-
-
-@handler.add(UnfollowEvent)
-def handle_unfollow():
-    app.logger.info("Got Unfollow event")
-
-
-@handler.add(JoinEvent)
-def handle_join(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text='Joined this ' + event.source.type))
-
-
-@handler.add(LeaveEvent)
-def handle_leave():
-    app.logger.info("Got leave event")
-
-
-@handler.add(PostbackEvent)
-def handle_postback(event):
-    if event.postback.data == 'ping':
-        line_bot_api.reply_message(
-            event.reply_token, TextSendMessage(text='pong'))
-    elif event.postback.data == 'datetime_postback':
-        line_bot_api.reply_message(
-            event.reply_token, TextSendMessage(text=event.postback.params['datetime']))
-    elif event.postback.data == 'date_postback':
-        line_bot_api.reply_message(
-            event.reply_token, TextSendMessage(text=event.postback.params['date']))
-
-
-@handler.add(BeaconEvent)
-def handle_beacon(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(
-            text='Got beacon event. hwid={}, device_message(hex string)={}'.format(
-                event.beacon.hwid, event.beacon.dm)))
-
+    if(opentime3 == ''):
+        opentime3 = '営業時間：情報がありません'
+    if(name3 == ''):
+        name3 = '情報がありません'
+    if(address3 == ''):
+        address3 = '住所：情報がありません'
+    if(url3 == ''):
+        url3 = 'https://line.me'
+    if(thumbnail3 == ''):
+        thumbnail3 = 'https://example.com/cafe.jpg'
+    print(tel1)
+    print(tel2)
+    print(tel3)
+    carousel_template = CarouselTemplate(columns=[
+        CarouselColumn(
+            thumbnail_image_url=thumbnail1,
+            text=opentime1, 
+            title=name1, 
+            actions=[
+                URIAction(label='詳細を表示', uri=url1),
+                URIAction(label='電話', uri=tel1),
+                MessageAction(label='住所を表示', text=address1)
+        ]),
+        CarouselColumn(
+            thumbnail_image_url=thumbnail2,
+            text=opentime2, 
+            title=name2, 
+            actions=[
+                URIAction(label='詳細を表示', uri=url2),
+                URIAction(label='電話', uri=tel2),
+                MessageAction(label='住所を表示', text=address2)
+        ]),        
+        CarouselColumn(
+            thumbnail_image_url=thumbnail3,
+            text=opentime3, 
+            title=name3, 
+            actions=[
+                URIAction(label='詳細を表示', uri=url3),
+                URIAction(label='電話', uri=tel3),
+                MessageAction(label='住所を表示', text=address3)
+        ]),
+    ])
+    template_message = TemplateSendMessage(
+        alt_text='お店が見つかりました', template=carousel_template)
+    return template_message
 
 if __name__ == "__main__":
-    app.run(debug=options.debug, port=options.port)
+    arg_parser = ArgumentParser(
+        usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
+    )
+    arg_parser.add_argument('-p', '--port', type=int, default=8000, help='port')
+    arg_parser.add_argument('-d', '--debug', default=False, help='debug')
+    options = arg_parser.parse_args()
 
+    # create tmp dir for download content
+    make_static_tmp_dir()
+
+    app.run(debug=options.debug, port=options.port)
